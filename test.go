e6853d47155c5ba53c5f325d91c9e1bcc65a426f
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-05-25 21:46:46 +0200

    Add main test.

---
+++ test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello, world")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) // get request method
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("login.gtpl")
+		err := t.Execute(w, token)
+		if err != nil {
+			log.Fatal("template error: ", err)
+		}
+	} else {
+		// log in request
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			// check token validity
+		} else {
+			// give error if no token
+		}
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) // print in server side
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username"))) // respond to client
+	}
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld) // setting router rule
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil) // setting listening port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
